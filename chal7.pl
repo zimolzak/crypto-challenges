9e9a5a3c212bda52c8aec7d6da6a9597d4d5ca64
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-08-22 16:24:31 -0400

    Working clean AES-ECB decrypt (challenge 7)

--- chal7.pl
+++ chal7.pl
@@ -6,14 +6,12 @@
 #     Full notice is found in the file 'LICENSE' in the same directory
 #     as this file.
 
+# usage: ./chal7.pl 7.txt
+
 use strict;
 use MIME::Base64 qw(decode_base64);
 use Crypt::OpenSSL::AES;
 use Cryptopals qw(ascii2hex);
-use Crypt::CBC;
-#use Crypt::Mode::ECB;
-
-print Crypt::OpenSSL::AES->VERSION, "\n";
 
 my $ciphertext;
 while(<>){
@@ -24,28 +22,7 @@ while(<>){
 }
 
 my $key = "YELLOW SUBMARINE";
-
-warn "len = ", length($ciphertext), ". Len mod 16 = ",
-(length($ciphertext) % 16), ".\n";
-
-# can't use Crypt::OpenSSL::AES directly; wants EXACTLY 16 bytes.
-#      my $cipher = new Crypt::OpenSSL::AES($key);
-
-my $cipher = Crypt::CBC->new(
-    -key    => $key,
-    -cipher => "Crypt::OpenSSL::AES",
-#    -header => "none"
-    ); # depend on Crypt::DES ??
-
-my $test = $cipher->encrypt("Hiya\n");
-print "Secret! ", ascii2hex($test), "\n";
-print $cipher->decrypt($test);
-
 my $aes = new Crypt::OpenSSL::AES($key);
-
 for (my $i=0; my $block = substr($ciphertext, 16*$i, 16); $i++) {
     print $aes->decrypt($block);
 }
-
-print $cipher->decrypt($ciphertext);
-print "\n";
