bb63dc71ea03c0a746e261682e1b00d2bf8aed06
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-08-22 16:20:59 -0400

    Working, with a bunch of errors and diagnostics.

--- chal7.pl
+++ chal7.pl
@@ -10,7 +10,10 @@ use strict;
 use MIME::Base64 qw(decode_base64);
 use Crypt::OpenSSL::AES;
 use Cryptopals qw(ascii2hex);
-use Crypt::Mode::ECB; 
+use Crypt::CBC;
+#use Crypt::Mode::ECB;
+
+print Crypt::OpenSSL::AES->VERSION, "\n";
 
 my $ciphertext;
 while(<>){
@@ -31,8 +34,18 @@ warn "len = ", length($ciphertext), ". Len mod 16 = ",
 my $cipher = Crypt::CBC->new(
     -key    => $key,
     -cipher => "Crypt::OpenSSL::AES",
-    -header => "none"
+#    -header => "none"
     ); # depend on Crypt::DES ??
+
+my $test = $cipher->encrypt("Hiya\n");
+print "Secret! ", ascii2hex($test), "\n";
+print $cipher->decrypt($test);
+
+my $aes = new Crypt::OpenSSL::AES($key);
+
+for (my $i=0; my $block = substr($ciphertext, 16*$i, 16); $i++) {
+    print $aes->decrypt($block);
+}
 
 print $cipher->decrypt($ciphertext);
 print "\n";
