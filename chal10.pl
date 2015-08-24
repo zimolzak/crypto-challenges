d35e2df78f9c8580f1099d490d4291ef3af9be96
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-08-24 14:06:52 -0400

    Properly split challenge 11 from 10.

--- chal10.pl
+++ chal10.pl
@@ -11,7 +11,7 @@
 use strict;
 use MIME::Base64 qw(decode_base64);
 use Crypt::OpenSSL::AES;
-use Cryptopals qw(aes_cbc encrypt_randomly ascii2hex);
+use Cryptopals qw(aes_cbc);
 
 my $ciphertext;
 while(<>){
@@ -21,23 +21,10 @@ while(<>){
 }
 
 my $key = "YELLOW SUBMARINE";
-my $block = substr($ciphertext, 0, 16);
 my $iv = "\x00" x 16;
 print aes_cbc($key, $ciphertext, $iv, "dec");
 
 my @plaintextlines = split(/\n/, aes_cbc($key, $ciphertext, $iv, "dec"));
 die unless $plaintextlines[1] =~ /on the mike/;
 
-my $text = "Got me a movie, I want you to k\n";
-my $ciph = aes_cbc($key, $text, $iv, "enc");
-my $decrypt = aes_cbc($key, $ciph, $iv, "dec");
-die unless $text eq $decrypt;
-
-print "Passed assertions (challenge 10)\n";
-
-print ascii2hex(encrypt_randomly($text)), "\n";
-print ascii2hex(encrypt_randomly($text)), "\n";
-print ascii2hex(encrypt_randomly($text)), "\n";
-print ascii2hex(encrypt_randomly($text)), "\n";
-
-
+print "Passed assertions (challenge 10).\n";
---
+++ chal11.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+#     chal11.pl - ECB/CBC detection oracle
+# 
+#     Copyright (C) 2015 Andrew J. Zimolzak <andyzimolzak@gmail.com>
+#     Full notice is found in the file 'LICENSE' in the same directory
+#     as this file.
+
+use strict;
+use Crypt::OpenSSL::AES;
+use Cryptopals qw(aes_cbc encrypt_randomly ascii2hex);
+
+my $key = "YELLOW SUBMARINE";
+my $iv = "\x00" x 16;
+my $text = "Got me a movie, I want you to k\n";
+
+my $ciph = aes_cbc($key, $text, $iv, "enc");
+my $decrypt = aes_cbc($key, $ciph, $iv, "dec");
+die unless $text eq $decrypt;
+
+print ascii2hex(encrypt_randomly($text)), "\n";
+print ascii2hex(encrypt_randomly($text)), "\n";
+print ascii2hex(encrypt_randomly($text)), "\n";
+print ascii2hex(encrypt_randomly($text)), "\n";
+
+print "Passed assertions (challenge 11)\n";
