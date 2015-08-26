b46747334c076b42543618a72470abe3373ce0f7
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-08-25 23:53:57 -0400

    prep skeleton code for challenge 15

---
+++ chal15.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+#     chal15.pl - PKCS#7 padding validation
+# 
+#     Copyright (C) 2015 Andrew J. Zimolzak <andyzimolzak@gmail.com>
+#     Full notice is found in the file 'LICENSE' in the same directory
+#     as this file.
+
+use strict;
+
+# will use Error; throw Error::Simple "doh";
+
+warn "Passed assertions ($0)\n";
