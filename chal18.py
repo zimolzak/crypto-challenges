9988fe21801c94abadad066b40cd2cbe64788613
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-08-31 23:31:53 -0400

    tidy up

--- chal18.py
+++ chal18.py
@@ -15,9 +15,6 @@ ciphertext = base64.b64decode(
     "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
 key = "YELLOW SUBMARINE"
 nonce = "\x00\x00\x00\x00\x00\x00\x00\x00" # 8 byte nonce
-
-# print [ciphertext]
-
 cipher = AES.new(key, AES.MODE_ECB)
 plaintext = ""
 
@@ -27,22 +24,18 @@ keystream = ""
 bs = len(key)
 n_blocks = int(ceil(float(len(ciphertext)) / bs))
 
-#generate whole keystream
 for i in range(n_blocks):
     counter = int2str(i, bs - len(nonce), "little") # 8 byte counter
     # print([add_str(nonce,counter)]) # deleteme
     # print ([nonce+counter])
     keystream = keystream + cipher.encrypt(nonce + counter)
 
+# do the encrypt or decrypt
+
 for i in range(len(ciphertext)):
     # need this loop otherwise maybe differing lengths
     plaintext = plaintext + xor_str(keystream[i], ciphertext[i])
 
-#x = int2str(65534 * 256 + 1,8,"big")
-#print "i2s"
-#print [x]
-
-#print [keystream]
 print plaintext
 
 #### tests ####
