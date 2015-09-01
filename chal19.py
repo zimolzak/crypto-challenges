7fd77ef9d22eaa61428800e471a453f4ea6515c0
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-01 17:35:45 -0400

    Successful substitution decrypt of fixed-nonce CTR (challenge 19)

--- chal19.py
+++ chal19.py
@@ -16,29 +16,49 @@ for b64 in open('19.txt', 'r').read().splitlines():
     ciphertexts = ciphertexts + [ctr(base64.b64decode(b64),
                                     key, nonce, "little")]
 
-#print len(ciphertexts)
-
-# print transpose(ciphertexts[1], 16)
-
 guesses = [''] * 256
 for i in range(len(ciphertexts)):
+
+    # This loop is for manual use only. Serves no purpose in the final
+    # decryption step. Manually adjust the [i][15] number, and examine
+    # the printout for strings that look like slices of English text
+    # (mainly lowercase and spaces, few punctuation marks, no ASCII >
+    # 127). First char of each string is the J'th byte in the
+    # keystream.
+
     for c in range(256):
         if i == 0:
             guesses[c] = guesses[c] + chr(c)
-        guesses[c] = guesses[c] + chr(ord(ciphertexts[i][15]) ^ c) # 0..end
+        guesses[c] = guesses[c] + chr(ord(ciphertexts[i][15]) ^ c)
 
-for j in range(len(guesses)):
-    print [guesses[j]]
+# for j in range(len(guesses)):
+#    print [guesses[j]]
 
-# type in found keystream here
-keystream = "\xcf\x8b\x10,zc\x91J\xd1\xbc\xa96t\xcf\xb2\x87\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+# After finding the first 16 bytes of keystream, I easily deduced the
+# full text from Google. Now I can deduce the whole keystream from the
+# longest line, and then decrypt as follows:
+
+line00 = "I have met them at close of day"
+line04 = "I have passed with a nod of the head"
+line37 = "He, too, has been changed in his turn,"
+print "guesses:"
+print [xor_str(ciphertexts[0][0:len(line00)], line00)]
+print [xor_str(ciphertexts[4][0:len(line04)], line04)]
+print [xor_str(ciphertexts[37][0:len(line37)], line37)]
+
+keystream = xor_str(ciphertexts[37][0:len(line37)], line37)
+    
+plaintexts = [""] * len(ciphertexts)
 for i in range(len(ciphertexts)):
-    line = ""
     for j in range(len(ciphertexts[i])):
-        line = line + xor_str(ciphertexts[i][j], keystream[j])
-    print line
+        plaintexts[i] = plaintexts[i] + xor_str(ciphertexts[i][j],
+                                                keystream[j])
+
+print '\n'.join(plaintexts)
 
 #### tests ####
+
+assert plaintexts[1] == "Coming with vivid faces"
 
 assert len(ciphertexts) == 40
 
