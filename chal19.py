378fae19839c2e4107be5a30ef1f53c9be1efa2d
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-01 17:11:19 -0400

    found first 16 bytes of stream by hand

--- chal19.py
+++ chal19.py
@@ -7,7 +7,7 @@
 #     the file 'LICENSE' in the same directory as this file.
 
 import base64
-from cryptopals import warn, ctr
+from cryptopals import warn, ctr, xor_str
 
 key = open('unknown_key.txt', 'r').read().splitlines()[0]
 nonce = "\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -16,9 +16,27 @@ for b64 in open('19.txt', 'r').read().splitlines():
     ciphertexts = ciphertexts + [ctr(base64.b64decode(b64),
                                     key, nonce, "little")]
 
-print len(ciphertexts)
+#print len(ciphertexts)
 
+# print transpose(ciphertexts[1], 16)
 
+guesses = [''] * 256
+for i in range(len(ciphertexts)):
+    for c in range(256):
+        if i == 0:
+            guesses[c] = guesses[c] + chr(c)
+        guesses[c] = guesses[c] + chr(ord(ciphertexts[i][15]) ^ c) # 0..end
+
+for j in range(len(guesses)):
+    print [guesses[j]]
+
+# type in found keystream here
+keystream = "\xcf\x8b\x10,zc\x91J\xd1\xbc\xa96t\xcf\xb2\x87\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+for i in range(len(ciphertexts)):
+    line = ""
+    for j in range(len(ciphertexts[i])):
+        line = line + xor_str(ciphertexts[i][j], keystream[j])
+    print line
 
 #### tests ####
 
