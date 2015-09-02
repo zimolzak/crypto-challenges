6f10d37377cd6d4b925a032b13a0f3f6fe8a03f3
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-09-02 01:05:15 -0400

    tidy up

--- chal20.py
+++ chal20.py
@@ -17,15 +17,12 @@ for b64 in open('20.txt', 'r').read().splitlines():
     ciphertexts = ciphertexts + [ctr(base64.b64decode(b64),
                                     key, nonce, "little")]
 
-minimum = 9999
+#### Decrypt the hard way (statistics) ####
+
 maximum = 0
 for i in range(len(ciphertexts)):
-    if len(ciphertexts[i]) < minimum:
-        minimum = len(ciphertexts[i])
     if len(ciphertexts[i]) > maximum:
         maximum = len(ciphertexts[i])
-
-#print "max", maximum
 
 keystream = ""
 for k in range(maximum):
@@ -38,21 +35,14 @@ for k in range(maximum):
     a = break_cipher_given_keysize([1], concat, xor_char_str)
     keystream += a[0][0]
 
-#print
-#print "Keystream:", a[0]
-#print "First", minimum, "bytes of plaintext", a[1]
-#keystream = a[0][0]
-
 keystream = "\xcf" + keystream # somehow it misses the 1st byte
 
 # first 95 bytes are ok
 
-#print [keystream[96:]]
-#keystream = keystream[0:95] + xor_str(xor_str(keystream[95],'j'), 'k') + keystream[96:]
-#keystream = keystream[0:96] + xor_str(xor_str(keystream[96],'"'), 'n') + keystream[97:]
-
 for c in ciphertexts:
     print xor_uneq(c, keystream)
+
+#### Decrypt using Google ####
 
 longline46 = "I used to roll up, this is a hold up, ain't nuthin' funny / Stop smiling, be still, don't nuthin' move but the money"
 keystream = xor_uneq(ciphertexts[46], longline46)
