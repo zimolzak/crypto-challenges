f69a38158b80a4d7ddd13091d22544fb41796886
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-09-02 22:00:27 -0400

    Ineffective 1st try at reverse temper

--- chal23.py
+++ chal23.py
@@ -7,10 +7,29 @@
 #     the file 'LICENSE' in the same directory as this file.
 
 from cryptopals import warn
-from myrand import MTRNG
+from myrand import MTRNG, c, b
 
 m = MTRNG(67812)
-print m.extract_number()
+#print m.extract_number()
+
+y4 = 0xe016575d
+y3 = y4 ^ (y4 >> 18)
+
+y2_r15 = y3 & (2**15 - 1)
+y2_ls_t = y2_r15 << 15    # first octet unknown
+y2_ls_andc = y2_ls_t & c  # first octet unknown
+y2 = y3 ^ y2_ls_andc
+
+y1_r7 = y2 & (2**7 - 1)
+y1_ls_s = y1_r7 << 7      # first 9 octets unknown, 14 bit known
+y1_ls_andb = y1_ls_s & b  # first 9 octets unknown
+y1 = y2 ^ y1_ls_andb
+
+y0 = y1 ^ (y1 >> 11)      # only 3 bit known! 
+
+print hex(y0)
+
+
 
 #### tests, if any ####
 warn("No errors:", __file__)
