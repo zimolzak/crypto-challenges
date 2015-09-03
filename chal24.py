4aa3baeb1685de9ebf5fdf50b2c1dc86d530b6bd
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-09-03 18:59:30 -0400

    successful decrypt of 16 bit MT stream key

--- chal24.py
+++ chal24.py
@@ -10,13 +10,16 @@ from cryptopals import warn, xor_str
 from myrand import MTRNG
 import random
 
+#### Construct a plaintext
+
 letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 plaintext = ''
 for i in range(random.randint(1,20)):
     plaintext += random.choice(letters)
 plaintext += 'A' * 14
-
 print plaintext
+
+#### Encrypt it and test decryption.
 
 def msc(text, seed):
     """Mersenne stream cipher"""
@@ -32,13 +35,33 @@ def msc(text, seed):
         output += chr(keystream_num ^ ord(text_char))
     return output
 
-key = random.randint(0,255)
-    
+key = random.randint(0,65535)
 ciphertext = msc(plaintext, key)
-
 decipher = msc(ciphertext, key)
 print decipher
 
+#### Brute-force the encryption 
+
+winning_key = 0
+for keytry in range(65536):
+    # Seems to brute-force 1000 per 3 sec. Worst case 3.28 min.
+    decipher_try = msc(ciphertext, keytry)
+    if keytry % 1000 == 0:
+        print keytry
+    if decipher_try[-14:] == "A" * 14:
+        print "Broken with key", keytry
+        winning_key = keytry
+        break
+
+winning_decrypt = msc(ciphertext, winning_key)
+print winning_decrypt
+
+# Next step: generate a random "password reset token" using MT19937
+# seeded from the current time. Write a function to check if any given
+# password token is actually the product of an MT19937 PRNG seeded
+# with the current time.
+
 #### tests, if any ####
 assert decipher == plaintext
+assert winning_decrypt == plaintext
 warn("Passed assertions:", __file__)
