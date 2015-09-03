918e99a0ab4395b4f4cf15e48afb97ee641fceb6
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-03 13:06:24 -0400

    Turned Untemper() into proper function

--- chal23.py
+++ chal23.py
@@ -7,45 +7,46 @@
 #     the file 'LICENSE' in the same directory as this file.
 
 from cryptopals import warn
-from myrand import MTRNG, c, b
+from myrand import MTRNG, c, b, u, s, t, l
 
 m = MTRNG(67812)
 #print m.extract_number()
 
-y4 = 0xe016575d
+def untemper(y4):
 
-#### begin "untemper" ####
+    y3 = y4 ^ (y4 >> l)
 
-y3 = y4 ^ (y4 >> 18)
+    y2 = y3 & (2**t - 1)     # 15 bit
+    y2_ls = y2 << t          # 30 bit
+    y2_ls_andc = y2_ls & c
+    y2 = y3 ^ y2_ls_andc
+    y2_ls = y2 << t          # 32 bit (45)
+    y2_ls_andc = y2_ls & c
+    y2 = y3 ^ y2_ls_andc
 
-y2 = y3 & (2**15 - 1)     # 15 bit
-y2_ls = y2 << 15          # 30 bit
-y2_ls_andc = y2_ls & c
-y2 = y3 ^ y2_ls_andc
-y2_ls = y2 << 15          # 32 bit (45)
-y2_ls_andc = y2_ls & c
-y2 = y3 ^ y2_ls_andc
+    y1 = y2 & (2**s - 1)     # 7 bit
+    y1_ls = y1 << s          # 14 bit
+    y1_ls_andb = y1_ls & b
+    y1 = y2 ^ y1_ls_andb
+    y1_ls = y1 << s          # 21 bit
+    y1_ls_andb = y1_ls & b
+    y1 = y2 ^ y1_ls_andb
+    y1_ls = y1 << s          # 28 bit
+    y1_ls_andb = y1_ls & b
+    y1 = y2 ^ y1_ls_andb
+    y1_ls = y1 << s          # 32 bit (35)
+    y1_ls_andb = y1_ls & b
+    y1 = y2 ^ y1_ls_andb
 
-y1 = y2 & (2**7 - 1)      # 7 bit
-y1_ls = y1 << 7           # 14 bit
-y1_ls_andb = y1_ls & b
-y1 = y2 ^ y1_ls_andb
-y1_ls = y1 << 7           # 21 bit
-y1_ls_andb = y1_ls & b
-y1 = y2 ^ y1_ls_andb
-y1_ls = y1 << 7           # 28 bit
-y1_ls_andb = y1_ls & b
-y1 = y2 ^ y1_ls_andb
-y1_ls = y1 << 7           # 32 bit (35)
-y1_ls_andb = y1_ls & b
-y1 = y2 ^ y1_ls_andb
+    y0 = y1                  # 11 bit (highest bits)
+    y0 = y1 ^ (y0 >> u)      # 22 bit
+    y0 = y1 ^ (y0 >> u)      # 32 bit (33)
 
-y0 = y1                  # 11 bit (highest bits)
-y0 = y1 ^ (y0 >> 11)     # 22 bit
-y0 = y1 ^ (y0 >> 11)     # 32 bit (33)
+    return y0
 
-print "y0", hex(y0)
+answer = untemper(0xe016575d)
+print hex(answer)
 
 #### tests, if any ####
-assert y0 == 0xdeadbeef
+assert answer == 0xdeadbeef
 warn("Passed assertions:", __file__)
