cfb972848cb3fbc28baf43c6690859a2d6087520
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-05 01:22:52 -0400

    Get plaintext from "rand access" CTR (end challenge 25)

--- chal25.py
+++ chal25.py
@@ -6,12 +6,16 @@
 #     and licensed under GNU GPL version 3. Full notice is found in
 #     the file 'LICENSE' in the same directory as this file.
 
-from cryptopals import warn
+from cryptopals import warn, xor_str
 from fakeserver import ctr_ciphertext, edit_public, ctr_cheat
 
+# test editing
 edited = edit_public(ctr_ciphertext, 4, 'COOL')
 lines = ctr_cheat(edited).splitlines()[0:4]
-print '\n'.join(lines)
+
+# use editing to get keystream & thus plaintext
+keystream = edit_public(ctr_ciphertext, 0, "\x00" * len(ctr_ciphertext))
+print xor_str(keystream, ctr_ciphertext)
 
 #### tests, if any ####
 assert lines[0] == "I'm COOL and I'm ringin' the bell "
