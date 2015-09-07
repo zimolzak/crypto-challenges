3e1fd659c29123059fc11bb9d17beded9779078a
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-09-07 18:31:07 -0400

    Automate the tests of SHA1

--- chal28.py
+++ chal28.py
@@ -6,9 +6,9 @@
 #     and licensed under GNU GPL version 3. Full notice is found in
 #     the file 'LICENSE' in the same directory as this file.
 
-from cryptopals import warn, sha1, leftrotate, str2int, unknown_key as k
+from cryptopals import warn, sha1, leftrotate, str2int, unknown_key as k_true
 
-m = 'Vanilla'
+m_true = 'Vanilla'
 
 def secret_prefix_mac(message, key):
     assert len(key) == 16
@@ -18,17 +18,26 @@ def secret_prefix_mac(message, key):
     else:
         return string[2:]
 
-print "Message\t\t\t", secret_prefix_mac(m, k)
-print "Tampered message\t", secret_prefix_mac('vanilla', k)
-print "Tampered message\t", secret_prefix_mac('Vanilla ', k)
-print "Tampered message\t", secret_prefix_mac('Vanilla\x00', k)
-print "Tampered message\t", secret_prefix_mac('Vanille', k)
-print "Lost key\t\t", secret_prefix_mac(m, 'YELLOW SUBMARINE')
-print "Lost key\t\t", secret_prefix_mac(m, 'abcdefghijklmnop')
-print "Lost key\t\t", secret_prefix_mac(m, '1234567890123456')
-print "Lost key\t\t", secret_prefix_mac(m, '                ')
-print "Lost key\t\t", secret_prefix_mac(m, '_______--_______')
-print "Lost key\t\t", secret_prefix_mac(m, '\x00' * 16)
+m_tamper = ['vanilla', 'Vanilla ', 'Vanilla\x00', 'Vanille']
+lost_key = ['YELLOW SUBMARINE',
+            'abcdefghijklmnop',
+            '1234567890123456',
+            '                ',
+            '_______--_______',
+            '\x00' * 16]
+
+authentic = secret_prefix_mac(m_true, k_true)
+print "Message\t\t\t", authentic
 
 #### tests, if any ####
+for m in m_tamper:
+    mac = secret_prefix_mac(m, k_true)
+    print "Tampered message\t", mac
+    assert mac != authentic
+
+for l in lost_key:
+    mac = secret_prefix_mac(m_true, l)
+    print "Lost key\t\t", mac
+    assert mac != authentic
+
 warn("Passed assertions:", __file__)
