6b7764cc2964a2ac5d3ad31917ab0f2cf0223245
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-08 13:26:24 -0400

    Successfully re-starting SHA but probably doing "final padding" wrong

--- chal29.py
+++ chal29.py
@@ -8,16 +8,36 @@
 
 from cryptopals import (warn, secret_prefix_mac, sha1, sha_padding,
                         unknown_key as k_true)
+from sha_analysis import restart_sha
+
+def i2h(n):
+    string = hex(n)
+    if string[-1] =="L":
+        return string[2:-1]
+    else:
+        return string[2:]
 
 m = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+auth_code = sha1(k_true + m)
+print "Message", m
+print "Auth code for M:       ", i2h(auth_code)
+print "Story checks out?      ", auth_code == sha1(k_true + m) 
+print
 
-authentic = secret_prefix_mac(m, k_true)
-print "Message\t\t\t", authentic
-print "Padding:"
-print [sha_padding(m)]
-print "Unkeyed", hex(sha1(m))
-print "Unkeyed  ", secret_prefix_mac(m, "")
+#### Mallory starts here
 
+adm = ";admin=true"
+nm = m + sha_padding(("A" * 16) +  m) + adm
+nac = i2h(restart_sha(auth_code, adm))
+print "New Message", [nm]
+print "Guess auth code for NM:", nac
+print "Story checks out?      ", nac == sha1(k_true + nm)
+print
+
+print "Cheat                  ", [sha_padding(k_true + m)]
+print "Cheat                  ", i2h(sha1(k_true + nm))
+nmcheat = m + sha_padding(k_true + m) + adm
+print "Are we guessing glue padding right?", nmcheat == nm
 
 #### tests, if any ####
 warn("Passed assertions:", __file__)
--- sha_analysis.py
+++ sha_analysis.py
@@ -5,10 +5,16 @@
 #     and licensed under GNU GPL version 3. Full notice is found in
 #     the file 'LICENSE' in the same directory as this file.
 
-from cryptopals import str2int, leftrotate, warn
+from cryptopals import warn, sha_fixated
 import math
 
-
+def restart_sha(hh, newmessage):
+    h0 = (hh >> 128) & 0xffffffff
+    h1 = (hh >> 96) & 0xffffffff
+    h2 = (hh >> 64) & 0xffffffff
+    h3 = (hh >> 32) & 0xffffffff
+    h4 = hh & 0xffffffff
+    return sha_fixated(newmessage, h0, h1, h2, h3, h4) #fixme
 
 #### tests ####
 warn("Passed assertions (" + __file__ + ")")
