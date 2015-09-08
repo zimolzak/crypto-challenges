d619cda82e159482794eaa249ea99133d9fa3983
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-08 18:53:10 -0400

    Using the restart_md4() function, but its internals are scrambled.

--- chal30.py
+++ chal30.py
@@ -6,8 +6,8 @@
 #     and licensed under GNU GPL version 3. Full notice is found in
 #     the file 'LICENSE' in the same directory as this file.
 
-from cryptopals import (warn, i2h, unknown_key as key)
-from py_md4.md4 import md4, md4_padding
+from cryptopals import warn, unknown_key as key
+from py_md4.md4 import md4, md4_padding, restart_md4
 import math
 
 message = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
@@ -32,17 +32,24 @@ adm = ";admin=true"
 new_message = message + glue_guess + adm
 print "New message                      ", new_message
 
+#delete next block
+print
+print 'lm', len(message)
+print "lnm", len(new_message)
+print
+
 # Construct the MAC for that message.
 KOG_len_guess = int(math.ceil((keylen_guess + len(message)) / 64.0)) * 64
   # Len of key+original+glue
 print "Key + original + glue length =   ", KOG_len_guess
-quit() # deleteme
-new_auth_code = restart_sha(auth_code, adm, KOG_len_guess)
-print "Guessed auth code for new message ", i2h(new_auth_code)
+auth_code = int(auth_code_str, 16)
+new_auth_code = restart_md4(auth_code, adm, KOG_len_guess, debug=True)
+print "Guessed auth code for new message", new_auth_code
+print "cheat                            ", md4(key + new_message, debug=True) #deleteme
 
 # Check the constructed MAC.
-i_am_a_winner = (new_auth_code == sha1(key + new_message))
-print "Story checks out?                 ", i_am_a_winner
+i_am_a_winner = (new_auth_code == md4(key + new_message))
+print "Story checks out?                ", i_am_a_winner
 print
 print "New Message (with unprintables)", [new_message]
 print
