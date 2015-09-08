77c2b59aca64a3069001802271417875446d14e9
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-08 11:27:12 -0400

    Functions have moved out of sha_analysis file.

--- sha_analysis.py
+++ sha_analysis.py
@@ -8,23 +8,7 @@
 from cryptopals import str2int, leftrotate, warn
 import math
 
-def sha_padding(message):
-    assert type(message) == type(str())
-    ml = 8 * len(message) # ML is in bits
-    n_bytes_to_add = (448 - (ml % 512)) / 8
-    padding_string = ""
-    for i in range(n_bytes_to_add):
-        if i > 0:
-            padding_string += '\x00'
-        else:
-            padding_string += '\x80'
-    for i in range(8):
-        byte_val = ml >> (64 - 8 * (i + 1)) & 0xff
-        # Big endian means R shift by 56, 48, ... , 8, 0.
-        padding_string += chr(byte_val)
-    message += padding_string
-    assert len(message) % (512/8) == 0
-    return padding_string
+
 
 #### tests ####
 warn("Passed assertions (" + __file__ + ")")
