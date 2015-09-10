72396734027d4fb4e2589e64f3643cd859e61458
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-10 15:32:19 -0400

    Factor out some fixed strings, to allow finding sig of any file.

--- chal31.py
+++ chal31.py
@@ -3,6 +3,10 @@
 #     chal31.py - Query a web app and use timing leak to discover
 #     valid MAC for any file, without knowing key.
 #
+#     Theoretical runtime is: 0.050 s/char * 8 (for failed attempts) *
+#     (\sum_{i=1}^{n} i) char. This is 5.4667 min when n=40. Obviously
+#     O(n^2). In practice it runs 7-8 min on my MacBook Pro.
+#
 #     Copyright (C) 2015 Andrew J. Zimolzak <andyzimolzak@gmail.com>,
 #     and licensed under GNU GPL version 3. Full notice is found in
 #     the file 'LICENSE' in the same directory as this file.
@@ -11,18 +15,17 @@ import urllib2
 import time
 from cryptopals import warn
 
-# 'bad&signature=605414df80961f70aff091df8e38d4cac526df99',
+# http://0.0.0.0:8080/test?file=bad&signature=605414df80961f70aff091df8e38d4cac526df99
 
-stubs = ['bad&signature=',
-         'bad&signature=6',
-         'bad&signature=b',
-         'bad&signature=a0',
-         'bad&signature=60',
-         'bad&signature=b0'
+stubs = ['http://0.0.0.0:8080/test?file=bad&signature=',
+         'http://0.0.0.0:8080/test?file=bad&signature=6',
+         'http://0.0.0.0:8080/test?file=bad&signature=b',
+         'http://0.0.0.0:8080/test?file=bad&signature=a0',
+         'http://0.0.0.0:8080/test?file=bad&signature=60',
+         'http://0.0.0.0:8080/test?file=bad&signature=b0'
 ]
 
-def time_stub(s):
-    url = 'http://0.0.0.0:8080/test?file=' + s
+def time_stub(url):
     start = time.time()
     try:
         response_obj = urllib2.urlopen(url)
@@ -30,7 +33,7 @@ def time_stub(s):
         n = time.time()
         return round(1000 * (n - start), 1)
     else:
-        return s
+        return url
 
 for x in stubs:
     print time_stub(x)
@@ -48,9 +51,9 @@ def next_char(urlstub, found_chars):
     ltime = None
     for hc in hexchars:
         if found_chars == "":
-            stub = 'bad&signature=' + found_chars + hc + 'z'
+            stub = urlstub + found_chars + hc + 'z'
         else:
-            stub = 'bad&signature=' + found_chars + hc + 'z'
+            stub = urlstub + found_chars + hc + 'z'
         ntime = time_stub(stub)
         if type(ntime) == type(str()):
             raise SuccessfulBreak(ntime)
@@ -69,7 +72,7 @@ def next_char(urlstub, found_chars):
     # only reaches here if all look equal. Try without 'z'
     ltime = None
     for hc in hexchars:
-        stub = 'bad&signature=' + found_chars + hc
+        stub = urlstub + found_chars + hc
         ntime = time_stub(stub)
         if type(ntime) == type(str()):
             raise SuccessfulBreak(ntime)
@@ -86,20 +89,20 @@ def next_char(urlstub, found_chars):
             print ntime, (ntime-ltime), stub
             ltime = ntime
 
+#all_chars = "605414df80961f70aff091df8e38d4cac526df9"
 all_chars = ""
 final = ""
 while(1):
     try:
-        all_chars += next_char('http://0.0.0.0:8080/test?file=bad&signature=',
+        all_chars += next_char('http://0.0.0.0:8080/test?file=noclist&signature=',
                                all_chars)
     except SuccessfulBreak as b:
         print "Hooray!", b
         final = str(b)
         break
 
-url = 'http://0.0.0.0:8080/test?file=' + final
-print url
-response_obj = urllib2.urlopen(url)
+print final
+response_obj = urllib2.urlopen(final)
 winner = 0
 for line in response_obj.read().splitlines():
     winner += 'winner' in line
