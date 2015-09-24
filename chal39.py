a27b989e1c4c4e94685a6cf2fc48a5ea43c07a45
Andy Zimolzak on linux iMac <andyzimolzak@gmail.com>
2015-09-23 23:19:04 -0400

    Faster prime finding, but works only sometimes.

--- chal39.py
+++ chal39.py
@@ -8,6 +8,9 @@
 
 from cryptopals import warn
 import random
+import numpy
+import gensafeprime
+from math import log, ceil
 
 def gen_primes():
     """ Generate an infinite sequence of prime numbers.
@@ -24,6 +27,18 @@ def gen_primes():
                 D.setdefault(p + q, []).append(p)
             del D[q]
         q += 1
+
+def primesfrom2to(n):
+    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = numpy.ones(n/3 + (n%6==2), dtype=numpy.bool)
+    sieve[0] = False
+    for i in xrange(int(n**0.5)/3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[      ((k*k)/3)      ::2*k] = False
+            sieve[(k*k+4*k-2*k*(i&1))/3::2*k] = False
+    return numpy.r_[2,3,((3*numpy.nonzero(sieve)[0]+1)|1)]
 
 def extended_gcd(a, b):
     s = 0;    old_s = 1
@@ -46,14 +61,35 @@ def invmod(a, m):
         ans += m
     return ans
 
+def rwh_primes2(n):
+    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Input n>=6, Returns a list of primes, 2 <= p < n """
+    correction = (n%6>1)
+    n = {0:n,1:n-1,2:n+4,3:n+3,4:n+2,5:n+1}[n%6]
+    sieve = [True] * (n/3)
+    sieve[0] = False
+    for i in xrange(int(n**0.5)/3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      ((k*k)/3)      ::2*k]=[False]*((n/6-(k*k)/6-1)/k+1)
+        sieve[(k*k+4*k-2*k*(i&1))/3::2*k]=[False]*((n/6-(k*k+4*k-2*k*(i&1))/6-1)/k+1)
+    return [2,3] + [3*i+1|1 for i in xrange(1,n/3-correction) if sieve[i]]
+
 def prime_greater(x):
-    for i in gen_primes():
-        if i > x:
-            return i
+#### Numpy
+#    return primesfrom2to(x)[-1]
+#### Standard
+#    for i in gen_primes():
+#        if i > x:
+#            return i
+#### RWH
+#    return rwh_primes2(x)[-1]
+    bits = int(ceil(log(x) / log(2)))
+    return gensafeprime.generate(bits)
 
 def keypair(maximum):
-    p = prime_greater(random.randint(2,maximum))
-    q = prime_greater(random.randint(2,maximum))
+    p = prime_greater(maximum)
+    q = prime_greater(maximum)
     n = p * q
     et = (p-1) * (q-1)
     for test in gen_primes():
@@ -85,35 +121,32 @@ def i2s(integer):
     f.reverse()
     return ''.join(f)
 
+print prime_greater(10 ** 10)
+    
 hello = 'Hiya'
 integer = s2i(hello)
-print integer
-string = i2s(integer)
-print string
-
 U, R = keypair(10 ** 5)
 ciphertext = crypt(integer, U)
-print ciphertext
-decrypt = crypt(ciphertext, R)
-print decrypt
-print "The answer is", i2s(decrypt)
-assert hello == i2s(decrypt)
+decrypt = i2s(crypt(ciphertext, R))
+print "Decrypted this message:", decrypt
+assert hello == decrypt
 
 #### tests ####
-for i in range(20):
+h = 'Hiya'
+assert i2s(s2i(h)) == h
+
+for i in range(10):
     U, R = keypair(10 ** 5)
     msg = random.randint(1,10000)
     ciphertext = crypt(msg, U)
     decrypt = crypt(ciphertext, R)
-    print "The answer is", decrypt
     assert msg == decrypt
 
-for i in range(20):
+for i in range(10):
     a = prime_greater(random.randint(2,100000))
     m = prime_greater(random.randint(2,100000))
     x = invmod(a, m)
     assert (a*x) % m == 1
 
-assert prime_greater(1000) == 1009
 assert invmod(17, 3120) == 2753
 warn("Passed assertions:", __file__)
