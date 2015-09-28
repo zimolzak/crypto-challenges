ad53575414c4e045284039cd0dab6420bea07a06
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-09-28 18:37:08 -0400

    Successful verification of an RSA signature.

--- chal42.py
+++ chal42.py
@@ -11,6 +11,7 @@ import rsa
 import random
 import cryptopals
 from hashlib import sha1
+from chal40 import find_cube_root
 
 message = 'Blah blah'
 
@@ -20,10 +21,11 @@ U, R = rsa.keypair(bits)
 hash = sha1("hi mom").digest()
 
 def pkcs_1_5(string, bits):
+    assert len(string) < 256
     assert bits % 8 == 0
     byte_goal = bits / 8
     prepend = "\x00\x01"
-    append = "\x00ASN.1"
+    append = "\x00ASN.1" + chr(len(string)) # not in real life
     bytes_to_add = (byte_goal -
                     (len(string) % byte_goal) -
                     len(prepend) -
@@ -31,17 +33,47 @@ def pkcs_1_5(string, bits):
     return prepend + ("\xff" * bytes_to_add) + append + string
 
 block = pkcs_1_5(hash, bits)
-signature = rsa.encrypt_string(block, R) # note sign w/ priv key
-verified = rsa.decrypt_string(signature, U) # verify w/ pub key
-for i in range((bits/8) - len(verified)):
-    verified = "\x00" + verified
-print [verified]
+signature = rsa.encrypt_string(block, R) # Note: signing is w/ priv key.
 
 def verify(sig, expected):
-    return False
+    block = rsa.decrypt_string(signature, U) # Verifying is w/ pub key.
+    for i in range((bits/8) - len(block)):
+        block = "\x00" + block
+    return sha1(expected).digest() == unpad(block)
+
+def unpad(string):
+    state = "start"
+    result = ""
+    for i, c in enumerate(string):
+        if i == 0 and c != "\x00":
+            return "FAIL " + str(i) + str([c]) + "_"
+        elif i == 0:
+            continue
+        if i == 1 and c != "\x01":
+            return "FAIL " + str(i)
+        elif i == 1:
+            continue
+        if i == 2 and c != "\xff":
+            return "FAIL " + str(i)
+        elif i == 2:
+            state = "ff bytes"
+        if state == "ff bytes" and c == "\xff":
+            continue
+        if state == "ff bytes" and c == "\x00":
+            # 0ASN.1l____
+            # i1234567
+            len_hash = ord(string[i+6])
+            loc_hash = i+7
+            # Error coming up! Does not check for garbage after hash!
+            return string[loc_hash : loc_hash + len_hash]
+        else:
+            return "FAIL " + str(i)
+
+print verify(signature, "hi mom")
 
 #### tests ####
-assert verified == block
+#assert verified == block
+assert unpad(pkcs_1_5("Hello", 1024)) == "Hello"
 assert len(block) == bits / 8
 assert len(pkcs_1_5("HELLO", 1024)) == 1024 / 8
 warn("Passed assertions:", __file__)
