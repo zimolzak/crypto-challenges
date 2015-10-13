cd25dda373981a3c09b65343818557ceec7ac354
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-10-13 17:13:39 -0400

    successful step 1 but messy

--- chal47.py
+++ chal47.py
@@ -8,6 +8,7 @@
 
 from cryptopals import warn
 import rsa
+from random import randint
 
 def pkcs_1(string, bits):
     """Pad a string to specified number of bits.
@@ -28,20 +29,53 @@ def pkcs_1(string, bits):
     return prepend + ("\xff" * bytes_to_add) + append + string
 
 def oracle(ciphertext, privkey, bits):
+    """bits should equal the max bits of a message, not bit length of
+    key.
+    """
     plaintext = rsa.decrypt_string(ciphertext, privkey)
+    #pt_n = pow(ciphertext, privkey[0], privkey[1])
+    #print pt_n
+    #assert pt_n < privkey[1]
+    #print [plaintext]
     assert bits % 8 == 0
     bytes = bits / 8
     diff = bytes - len(plaintext)
     plaintext = "\x00" * diff + plaintext
+    assert len(plaintext) == bytes, len(plaintext)
+    if plaintext[0] == "\x00":
+        print [plaintext[0:2]]
     return plaintext[0] == "\x00" and plaintext[1] == "\x02"
 
 Bits = 256
 pubkey, privkey = rsa.keypair(Bits)
 short_message = "kick it, CC"
-m = pkcs_1(short_message, Bits)
+m = pkcs_1(short_message, Bits * 2) # Bits*2 = length of n
 c = rsa.encrypt_string(m, pubkey)
 
-print "Oracle says that ciphertext conforms?", oracle(c, privkey, Bits)
+print "Oracle says that raw ciphertext conforms?", oracle(c, privkey, Bits * 2)
+
+#### Step 1.
+
+e = pubkey[0]
+n = pubkey[1]
+k = Bits * 2 / 8 # Length of n in bytes
+B = 2 ** (8 * (k - 2))
+print "Conforming plaintexts are between", hex(2 * B)[:10], "... and", hex(3 * B - 1)[:10], "...."
+
+
+
+for i in range(2**19): # Like 8x coverage of 2**16 (two bytes)
+    s0 = randint(2, 2**62) # not a Long int
+    x = c * pow(s0, e, n) % n # multiplies plaintext by s0
+    assert x < n
+    found_s0 = oracle(x, privkey, Bits * 2)
+    if i % 1000 == 0:
+        print i
+    if found_s0:
+        break
+
+print "success?", found_s0, s0
+
 
 #### tests ####
 short_message2 = "testing"
