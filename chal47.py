81abefb31511d13aed9b391dd2734bb810415774
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-10-13 18:58:41 -0400

    All steps SEEM to be coded, but without looping.

--- chal47.py
+++ chal47.py
@@ -10,6 +10,13 @@ from cryptopals import warn
 import rsa
 from random import randint
 from time import time
+import math
+
+def ceildiv(x, y):
+    if x % y == 0:
+        return x / y
+    else:
+        return x / y + 1
 
 def pkcs_1(string, bits):
     """Pad a string to specified number of bits.
@@ -42,76 +49,103 @@ def oracle(ciphertext, privkey, bits):
     return plaintext[0] == "\x00" and plaintext[1] == "\x02"
 
 Bits = 256
-pubkey, privkey = rsa.keypair(Bits)
+
+#pubkey, privkey = rsa.keypair(Bits)
+
+# Set to two static keys known to yield a low s1, for efficiency's
+# sake while testing. FIXME - delete these two static values maybe,
+# and reinstitute random keypair generation?
+
+pubkey = [3, 9735206716434150621826121115776169484354439000548572013769499204551711882059896149904093367744988308059944044877240224792294074570476059825549758422564041]
+
+privkey = [6490137810956100414550747410517446322902959333699048009179666136367807921373131849354707838340464847438729993288283438687391902142174646364993310402589795, 9735206716434150621826121115776169484354439000548572013769499204551711882059896149904093367744988308059944044877240224792294074570476059825549758422564041]
+
 short_message = "kick it, CC"
 m = pkcs_1(short_message, Bits * 2) # Bits*2 = length of n
 c = rsa.encrypt_string(m, pubkey)
 
 print "Oracle says that raw ciphertext conforms?", oracle(c, privkey, Bits * 2)
 
-#### Step 1.
+#### Step 1. Can be skipped if c is already PKCS conforming
 
 e = pubkey[0]
 n = pubkey[1]
 k = Bits * 2 / 8 # Length of n in bytes
 B = 2 ** (8 * (k - 2))
 print "Conforming plaintexts are between", hex(2 * B)[:10], "... and", hex(3 * B - 1)[:10], "...."
-start = time()
-s0 = None
-c0 = None
 
-for i in range(2**19): # Like 8x coverage of 2**16 (two bytes)
-    s0 = randint(2, 2**62) # not a Long int
-    c0 = c * pow(s0, e, n) % n # multiplies plaintext by s0
-    assert c0 < n
-    found_s0 = oracle(c0, privkey, Bits * 2)
-    if i % 10000 == 0: # rate usually 1000 per sec
-        print i
-    if found_s0:
-        break
+assert oracle(c, privkey, Bits*2)
+s = [1]
+c = [c]
+# M is a list of sets of intervals.
+M = [[[2*B, 3*B-1]]]
+i = 1
 
-end = time()
-print "Found s0?", found_s0, s0
-dur = end - start
-rate = i / dur
-print i, "guesses in", round(dur, 1), "sec for", round(rate,1), "per sec."
+#### Step 2
 
-#### Step 2.a
-
-s1 = n / (3 * B) # don't use range() or it breaks
-while s1 < n:
-    x = c0 * pow(s1, e, n) % n # multiplies plaintext_0 by s1
-    if oracle(x, privkey, Bits * 2):
-        break
-    s1 += 1
-
-print "Found s1?", oracle(x, privkey, Bits * 2), s1
-
-#### Step 2.c [ search w/ 1 interval left in O(log n) ]
-
-a = 2*B
-b = 3*B - 1
-
-j = 0
-
-r2 = 2 * (b*s1 - 2*B) / n # starts here & grows to...
-while r2 < n:
-    s2 = (2*B + r2*n)/b # starts here & grows to...
-    while s2 < (3*B + r2*n) / a:
-        x = c0 * pow(s2, e, n) % n # multiplies plaintext_0 by s2
+if i == 1:
+    print "Step 2.a"
+    s.append(int(n / (3 * B))) # Set s[1]. Don't use range() or it breaks.
+    while s[1] < n:
+        x = c[0] * pow(s[1], e, n) % n # multiplies plaintext_0 by s1
         if oracle(x, privkey, Bits * 2):
             break
-        if j % 1000 == 0:
-            print r2, s2
-        s2 += 1
-        j += 1
-    r2 += 1
+        if s[1] % 4000 == 0:
+            print s
+        s[1] += 1
+    print "Found s1?", oracle(x, privkey, Bits * 2), s
+elif i > 1 and len(M[i-1]) >= 2:
+    print "Step 2.b"
+    pass
+elif len(M[i-1]) == 1:
+    print "Step 2.c"
+    a = M[i-1][0][0]
+    b = M[i-1][0][1]
 
-print "Found s2?", oracle(x, privkey, Bits * 2), s2
+    r2 = 2 * (b*s1 - 2*B) / n # starts here & grows to...
+    while r2 < n:
+        s2 = (2*B + r2*n)/b # starts here & grows to...
+        while s2 < (3*B + r2*n) / a:
+            x = c0 * pow(s2, e, n) % n # multiplies plaintext_0 by s2
+            if oracle(x, privkey, Bits * 2):
+                break
+            s2 += 1
+        r2 += 1
+    print "Found s2?", oracle(x, privkey, Bits * 2), s2
+
+#### Step 3
+
+print "Step 3"
+m_set = []
+for [a,b] in M[i-1]:
+    rlow = (a * s[i] - 3*B + 1) / n
+    rhigh = (b * s[i] - 2*B) / n
+    for r in range(rlow, rhigh+1):
+        mlow = max(a, ceildiv(2*B + r*n, s[i]))
+        mhigh =  min(b, (3*B - 1 + r*n) / s[i])
+        if [mlow, mhigh] not in m_set:
+            m_set.append([mlow, mhigh])
+M.append(m_set)
+
+#### Step 4
+
+print "Step 4"
+if len(M[i]) == 1 and M[i][0][0] == M[i][0][1]:
+    a = M[i][0][0]
+    m = a * rsa.invmod(s[0], n) % n
+    print "hooray", n
+else:
+    print "go around again because..."
+    if len(M[i]) > 1:
+        print "len", len(M[i])
+    else:
+        print "range", M[i][0][0] - M[i][0][1]
+    i += 1
+    print
 
 #### tests ####
 short_message2 = "testing"
-m2 = pkcs_1(short_message2, Bits)
+m2 = pkcs_1(short_message2, Bits*2)
 c2 = rsa.encrypt_string(m2, pubkey)
-assert oracle(c2, privkey, Bits)
+assert oracle(c2, privkey, Bits*2)
 warn("Passed assertions:", __file__)
